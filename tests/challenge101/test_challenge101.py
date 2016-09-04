71d6b2e45a50cf94feedb5574bedcb3be44259bc
Matt Hussey <matthew.hussey@googlemail.com>
2016-09-04 22:59:05 +0100

    Add tests for challenge101

--- tests/challenge101/test_challenge101.py
+++ tests/challenge101/test_challenge101.py
@@ -5,8 +5,4 @@ from pemjh.challenge101 import main
 
 def test_challenge101():
     """ Regression testing challenge101 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge101_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(37076114526)

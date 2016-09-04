bc321b8f402d45f1029e66c237d5913970245b84
Matt Hussey <matthew.hussey@googlemail.com>
2016-09-04 17:59:36 +0100

    Add challenge9

---
+++ src/pemjh/challenge9/main.py
@@ -0,0 +1,19 @@
+""" Challenge9 """
+
+
+def main(limit):
+    """ challenge009 """
+    # a2 + b2 - c2 = 0
+
+    answer = 0
+
+    # a_side can be 1 up to 1/3 away from limit
+    for a_side in range(1, (limit / 3)):
+        # b_side can be 1 more than a_side up to half the remainder
+        remainder = limit - a_side
+        for b_side in range((a_side + 1), (a_side + 1 + (remainder / 2))):
+            c_side = limit - a_side - b_side
+            if (a_side**2 + b_side**2 - c_side**2) == 0:
+                answer = a_side * b_side * c_side
+
+    return answer
---
+++ tests/challenge9/test_challenge9.py
@@ -0,0 +1,13 @@
+""" Tests for challenge9 """
+import pytest
+from robber import expect
+from pemjh.challenge9 import main
+
+
+@pytest.mark.parametrize('input, expected',
+                         [
+                             (1000, 31875000)
+                         ])
+def test_challenge9(input, expected):
+    """ Regression testing challenge9 """
+    expect(main(input)).to.eq(expected)

608140be13fce3f89050c943b40c5743532d93bb
Matt Hussey <matthew.hussey@googlemail.com>
2016-09-07 21:03:06 +0100

    Add tests for challenge104

--- src/pemjh/challenge104/main.py
+++ src/pemjh/challenge104/main.py
@@ -1,13 +1,6 @@
 """ Challenge104 """
 
 
-def myEnumerate(seq, start=0):
-    i = start
-    for item in seq:
-        yield [i, item]
-        i += 1
-
-
 def fiboTrunc(trunc):
     aStart, bStart = 0, 1
     aEnd, bEnd = 0, 1
@@ -39,11 +32,14 @@ def fiboTrunc(trunc):
 def main():
     """ challenge104 """
     lowerLimit = 123456788  # Must be at least greater than this
-    for k, f in myEnumerate(([s, e] for s, e in fiboTrunc(9)), start=1):
-        s = f[0]
-        e = f[1]
+    found = None
+    endPointGen = fiboTrunc(9)
+    index = 0
+    while not found:
+        s, e = endPointGen.next()
+        index += 1
         if s > lowerLimit and e > lowerLimit and \
                 "".join(sorted(str(s))) == "123456789" and \
                 "".join(sorted(str(e))) == "123456789":
-            return k
-    return
+            found = index
+    return found
--- tests/challenge104/test_challenge104.py
+++ tests/challenge104/test_challenge104.py
@@ -5,8 +5,4 @@ from pemjh.challenge104 import main
 
 def test_challenge104():
     """ Regression testing challenge104 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge104_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(329468)

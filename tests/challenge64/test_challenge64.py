917a4571c550a586750580a7504ad85ecaed6eb0
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:23:28 +0000

    Add tests for challenge64

--- tests/challenge64/test_challenge64.py
+++ tests/challenge64/test_challenge64.py
@@ -5,8 +5,4 @@ from pemjh.challenge64 import main
 
 def test_challenge64():
     """ Regression testing challenge64 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge64_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(1322)

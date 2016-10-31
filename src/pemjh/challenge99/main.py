d7a15769317f7bf158e63bc4ca79acb0a227b8c4
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:34:03 +0000

    Add tests for challenge99

--- src/pemjh/challenge99/main.py
+++ src/pemjh/challenge99/main.py
@@ -1,15 +1,13 @@
 """ Challenge099 """
 from __future__ import with_statement
-from pkgutil import get_data
 
 
-def main():
+def main(numbers):
     """ challenge099 """
-    f = get_data(__name__, 'base_exp.txt').split('\n')
     highestIndex = 1.0
     highestNumber = 0
     highestLine = 0
-    for nL, l in enumerate(l.split(",") for l in f):
+    for nL, l in enumerate(l.split(",") for l in numbers):
         n, i = int(l[0]), float(l[1])
         rootedPower = highestIndex / i
         if highestNumber ** rootedPower < n:
--- tests/challenge99/test_challenge99.py
+++ tests/challenge99/test_challenge99.py
@@ -1,12 +1,12 @@
 """ Tests for challenge99 """
+from os.path import abspath, dirname, join
 from robber import expect
 from pemjh.challenge99 import main
 
 
 def test_challenge99():
     """ Regression testing challenge99 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge99_example():
-    expect(main(2)).to.eq(None)
+    numbers_path = join(dirname(abspath(__file__)), 'base_exp.txt')
+    with open(numbers_path, 'r') as numbers_file:
+        numbers = [s.strip() for s in numbers_file.readlines()]
+        expect(main(numbers)).to.eq(709)

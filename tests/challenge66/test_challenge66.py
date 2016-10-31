7e2082863fbd8b9cb42c1562a009716b2926c97d
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:23:57 +0000

    Add tests for challenge66

--- tests/challenge66/test_challenge66.py
+++ tests/challenge66/test_challenge66.py
@@ -5,8 +5,4 @@ from pemjh.challenge66 import main
 
 def test_challenge66():
     """ Regression testing challenge66 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge66_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(661)

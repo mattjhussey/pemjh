4507f14e27514224a6b52ffe09b7b19f51199bce
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:32:48 +0000

    Add tests for challenge94

--- tests/challenge94/test_challenge94.py
+++ tests/challenge94/test_challenge94.py
@@ -5,8 +5,4 @@ from pemjh.challenge94 import main
 
 def test_challenge94():
     """ Regression testing challenge94 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge94_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(518408346)

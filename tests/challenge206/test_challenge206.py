48cb9ea4f30297497b20ff13d86d22f99fb23423
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:03:53 +0000

    Add tests for challenge206

--- tests/challenge206/test_challenge206.py
+++ tests/challenge206/test_challenge206.py
@@ -5,8 +5,4 @@ from pemjh.challenge206 import main
 
 def test_challenge206():
     """ Regression testing challenge206 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge206_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(1389019170)

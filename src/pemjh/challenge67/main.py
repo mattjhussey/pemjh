f40c2a52c6843355126b0655e7d43a1f94b9bc41
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:24:12 +0000

    Add tests for challenge67

--- src/pemjh/challenge67/main.py
+++ src/pemjh/challenge67/main.py
@@ -1,14 +1,11 @@
 """ Challenge067 """
-from pkgutil import get_data
 from pemjh.numbers import get_triangle_route_length
 
 
-def main():
+def main(triangles):
     """ challenge067 """
     rows = []
-    # Read in the file
-    triangle_file = get_data(__name__, 'triangle.txt').split('\n')[:-1]
-    for line in triangle_file:
+    for line in triangles:
         # Create a row
         vals = [int(x) for x in line.split()]
         rows.append(vals)
--- tests/challenge67/test_challenge67.py
+++ tests/challenge67/test_challenge67.py
@@ -1,12 +1,12 @@
 """ Tests for challenge67 """
+from os.path import abspath, dirname, join
 from robber import expect
 from pemjh.challenge67 import main
 
 
 def test_challenge67():
     """ Regression testing challenge67 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge67_example():
-    expect(main(2)).to.eq(None)
+    triangle_path = join(dirname(abspath(__file__)), 'triangle.txt')
+    with open(triangle_path, 'r') as triangle_file:
+        triangles = [s.strip() for s in triangle_file.readlines()]
+        expect(main(triangles)).to.eq(7273)

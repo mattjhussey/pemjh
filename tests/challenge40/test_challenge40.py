f9067ed2ca4ef1673b56720f7c9f297186dbf94a
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:16:07 +0000

    Add tests for challenge40

--- tests/challenge40/test_challenge40.py
+++ tests/challenge40/test_challenge40.py
@@ -5,8 +5,4 @@ from pemjh.challenge40 import main
 
 def test_challenge40():
     """ Regression testing challenge40 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge40_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(210)

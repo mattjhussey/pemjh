6a38dbfa2a84c5be1e19c4415dcd24d02f0a4e8c
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 19:59:56 +0000

    Add tests for challenge187

--- tests/challenge187/test_challenge187.py
+++ tests/challenge187/test_challenge187.py
@@ -5,8 +5,4 @@ from pemjh.challenge187 import main
 
 def test_challenge187():
     """ Regression testing challenge187 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge187_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(17427258)

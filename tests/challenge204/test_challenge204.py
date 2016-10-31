2ff60a4e0c42b82d2b155d93d3ab1894332df310
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:03:13 +0000

    Add tests for challenge204

--- tests/challenge204/test_challenge204.py
+++ tests/challenge204/test_challenge204.py
@@ -5,8 +5,4 @@ from pemjh.challenge204 import main
 
 def test_challenge204():
     """ Regression testing challenge204 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge204_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(2944730)

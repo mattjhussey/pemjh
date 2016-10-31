354dfdd1edaeb08daee15de11c6b2598401f5901
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:14:06 +0000

    Add tests for challenge32

--- tests/challenge32/test_challenge32.py
+++ tests/challenge32/test_challenge32.py
@@ -5,8 +5,4 @@ from pemjh.challenge32 import main
 
 def test_challenge32():
     """ Regression testing challenge32 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge32_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(45228)

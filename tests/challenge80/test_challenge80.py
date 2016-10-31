23710f5a3837184fb9ca09d34db217b5c97d6c68
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:28:58 +0000

    Add tests for challenge80

--- tests/challenge80/test_challenge80.py
+++ tests/challenge80/test_challenge80.py
@@ -5,8 +5,4 @@ from pemjh.challenge80 import main
 
 def test_challenge80():
     """ Regression testing challenge80 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge80_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(40886)

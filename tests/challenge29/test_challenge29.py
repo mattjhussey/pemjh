fe3cc1702c025b56cfee0e0fe13b2685a9ae8d25
Matt Hussey <matthew.hussey@googlemail.com>
2016-10-31 20:13:21 +0000

    Add tests for challenge29

--- tests/challenge29/test_challenge29.py
+++ tests/challenge29/test_challenge29.py
@@ -5,8 +5,4 @@ from pemjh.challenge29 import main
 
 def test_challenge29():
     """ Regression testing challenge29 """
-    expect(main(1)).to.eq(None)
-
-
-def test_challenge29_example():
-    expect(main(2)).to.eq(None)
+    expect(main()).to.eq(9183)

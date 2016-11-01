8ce18c23f5fb073097ecd55f093882ae041358ef
Matt Hussey <matthew.hussey@googlemail.com>
2016-11-01 01:08:16 +0000

    Alternative solution to 216

--- src/pemjh/challenge216/main.py
+++ src/pemjh/challenge216/main.py
@@ -1,84 +1,39 @@
 """ Challenge216 """
-import math
-
-
-def l(a, b):
-    k = 1
-    while a:
-        v = 0
-        while a % 2 == 0:
-            v += 1
-            a /= 2
-        if v % 2 and b % 8 in (3, 5):
-            k = -k
-        if b > a:
-            if a % 4 == 3 and b % 4 == 3:
-                k = -k
-            b, a = a, b - a
-        else:
-            a -= b
-    return 0 if b > 1 else k
-
-
-def sq(a, p):
-    q, e = p - 1, 0
-    while q % 2 == 0:
-        e += 1
-        q //= 2
-    n = 2
-    while l(n, p) == 1:
-        n += 1
-    z = pow(n, q, p)
-    y, r, x = z, e, pow(a, (q - 1) // 2, p)
-    b = a * x ** 2 % p
-    x = a * x % p
-    while b % p != 1:
-        bb, m = b, 0
-        while bb != 1:
-            bb = bb ** 2 % p
-            m += 1
-        if m == r:
-            return 0
-        t = y
-        for _ in range(r - m - 1):
-            t = t ** 2 % p
-        y, r, x = t ** 2 % p, m, x * t % p
-        b = b * y % p
-    return x
-
-
-def generate_primes(nmax):
-    primes = int(math.sqrt(2 * nmax ** 2)) * [True]
-
-    primes[0:2] = [False, False]
-
-    for x in range(2, int(math.sqrt(len(primes)) + 1)):
-        if primes[x]:
-            for y in range(2 * x, len(primes), x):
-                primes[y] = False
-
-    return [i for i, b in enumerate(primes) if b]
 
 
 def main():
     """ challenge216 """
     nmax = 50000000
 
-    primes = set(generate_primes(nmax))
-    t = (nmax + 1) * [True]
+    total = 0
 
-    t[0:2] = [False, False]
+    p = [2 * n * n - 1 for n in xrange(2, nmax + 1)]
+    p = [0, 0] + p
 
-    primes.remove(2)
-
-    for x in primes:
-        s = sq((x + 1) // 2, x)
-        if s:
-            for i in range(s, len(t), x):
-                if 2 * i ** 2 - 1 != x:
-                    t[i] = False
-            for i in range(x - s, len(t), x):
-                if 2 * i ** 2 - 1 != x:
-                    t[i] = False
-
-    return sum(1 for i in range(len(t)) if t[i])
+    for n in xrange(2, nmax + 1):
+        t = p[n]
+        if t == 1:
+            continue
+        if t == (2 * n * n - 1):
+            total += 1
+        k = 0
+        keep_going = True
+        while keep_going:
+            k += 1
+            i = t * k - n
+            if i > nmax:
+                keep_going = False
+            else:
+                while p[i] % t == 0:
+                    p[i] /= t
+        keep_going = True
+        k = 0
+        while keep_going:
+            k += 1
+            i = t * k + n
+            if i > nmax:
+                keep_going = False
+            else:
+                while p[i] % t == 0:
+                    p[i] /= t
+    return total

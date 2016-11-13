414dc15eb614fc631fb2bfb46a597fcd3bec4744
Matt Hussey <matthew.hussey@googlemail.com>
2016-11-13 14:24:27 +0000

    Remove coverage from test. It can be run manually if wanted.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def main():
         classifiers=[
             "Development Status :: 1 - Planning"],
         setup_requires=['pytest-runner'],
-        tests_require=['pytest-bdd', 'pytest-cov', 'robber', 'pytest']
+        tests_require=['pytest-bdd', 'robber', 'pytest']
     )
 
 if __name__ == "__main__":

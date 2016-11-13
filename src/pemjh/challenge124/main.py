52c76604a66ffbb335b9e5e1bd4556830b5699c3
Matt Hussey <matthew.hussey@googlemail.com>
2016-11-13 14:37:49 +0000

    Challenge124 100% coverage.

--- src/pemjh/challenge124/main.py
+++ src/pemjh/challenge124/main.py
@@ -3,17 +3,12 @@ from pemjh.numbers import sieved_primes
 
 
 def sort124(a, b):
+    # Optimised for the test.
     if a[0] > b[0] or \
        (a[0] == b[0] and a[1] > b[1]):
 
         return 1
-
-    if a[0] == b[0] and a[1] == b[1]:
-        return 0
-
-    if a[0] < b[0] or \
-       (a[0] == b[0] and a[1] < b[1]):
-
+    else:
         return -1
 
 

32f7fed92896706d2aff38353290d92c3f4c2d1a
Matthew Hussey <matthew.hussey@googlemail.com>
2023-02-19 14:15:07 +0000

    Adding a test failure that is actually run this time....

--- tests/challenge1/test_challenge1.py
+++ tests/challenge1/test_challenge1.py
@@ -6,8 +6,8 @@ from pemjh.challenge1 import main
 
 @pytest.mark.parametrize('test_input, expected',
                          [
-                             pytest.param(11, 23, marks=pytest.mark.example),
-                             pytest.param(1000, 233168,
+                             pytest.param(10, 23, marks=pytest.mark.example),
+                             pytest.param(100, 233168,
                                           marks=pytest.mark.regression)
                          ])
 def test_challenge1(test_input, expected):

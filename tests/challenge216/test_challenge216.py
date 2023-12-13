859d9edd89fc9d02f51270b0f09cd04835618068
Matthew Hussey <matthew.hussey@googlemail.com>
2023-12-13 21:42:19 +0000

    Set Challenge216 to example (instead of sample)

--- tests/challenge216/test_challenge216.py
+++ tests/challenge216/test_challenge216.py
@@ -7,7 +7,7 @@ from pemjh.challenge216 import main
 @pytest.mark.parametrize(
     ("nmax", "expected"), [
         pytest.param(50000000, 5437849, marks=pytest.mark.fullresult),
-        pytest.param(10000, 2202, marks=pytest.mark.sample),
+        pytest.param(10000, 2202, marks=pytest.mark.example),
         (9, 6)]
 )
 def test_challenge216(nmax, expected):
